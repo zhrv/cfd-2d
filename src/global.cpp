ef279cc97f52abcd23a5fe40f9bf509ecaa12463
Ruslan Zhalnin <zhalnin@gmail.com>
2016-04-07 07:27:02 +0300

    справлен код RIM

--- src/global.cpp
+++ src/global.cpp
@@ -233,172 +233,174 @@ void rim_orig(double& RI, double& EI, double& PI, double& UI, double& VI, double
          double RB, double PB, double UB, double VB, double WB,
          double RE, double PE, double UE, double VE, double WE, double GAM) {
 
-// 	double AGAM  =  (GAM-1.0);
-// 	double BGAM  =  (2.0*sqrt(GAM/AGAM));
-// 	double CGAM  =  (1.0/GAM);
-// 	double DGAM  =  (2.0/AGAM);
-// 	double EGAM  =  (AGAM/(GAM+1.0));
-// 	double GGAM  =  (sqrt(GAM*AGAM));
-// 	double HGAM  =  (AGAM/2.0);
-// 	double FGAM  =  (3.0*GAM-1.0);
-// 	double OGAM  =  (AGAM/(2.0*GAM));
-// 	double QGAM  =  (GAM+1.0);
-// 	double PGAM  =  (QGAM/(2.0*GAM));
-// 	double RGAM  =  (4.0*GAM);
-// 	double SGAM  =  (GAM*AGAM);
-// 	double TGAM  =  (QGAM/2.0);
-// 	double UGAM  =  (sqrt(AGAM/GAM));
+	double AGAM  =  (GAM-1.0);
+	double BGAM  =  (2.0*sqrt(GAM/AGAM));
+	double CGAM  =  (1.0/GAM);
+	double DGAM  =  (2.0/AGAM);
+	double EGAM  =  (AGAM/(GAM+1.0));
+	double GGAM  =  (sqrt(GAM*AGAM));
+	double HGAM  =  (AGAM/2.0);
+	double FGAM  =  (3.0*GAM-1.0);
+	double OGAM  =  (AGAM/(2.0*GAM));
+	double QGAM  =  (GAM+1.0);
+	double PGAM  =  (QGAM/(2.0*GAM));
+	double RGAM  =  (4.0*GAM);
+	double SGAM  =  (GAM*AGAM);
+	double TGAM  =  (QGAM/2.0);
+	double UGAM  =  (sqrt(AGAM/GAM));
+
+	double US = 0.0, UF = 0.0;
+
+	double RF,RS,EF,ES,SBL,SFL,SSL,SEL,D;
+    double PPB,PKB,ZNB,F1,FS1,ZFB,PPE,PKE,ZNE,F2,FS2,ZFE,DP,
+           UBD,RUBD,UED,RUED,P;
+	double    eps=1.0e-5;
+	double    CB= sqrt(GAM*PB/RB);
+	double    CE= sqrt(GAM*PE/RE);
+	double    EB= CB*CB/SGAM;
+	double    EE= CE*CE/SGAM;
+	double    RCB=RB*CB;
+	double    RCE=RE*CE;
+	double    DU=UB-UE;
+      if (DU < -2.0*(CB+CE)/AGAM) {
+         //printf(" ATTENTION!!!  VACUUM \n");
+         RF=0.0;
+         RS=0.0;
+         EF=0.0;
+         ES=0.0;
+         SBL=UB-CB;
+         SFL=UB+2.0*CB/AGAM;
+         SSL=UE-2.0*CE/AGAM;
+         SEL=UE+CE;
+         goto lbl9;
+      }
+      P=(PB*RCE+PE*RCB+DU*RCB*RCE)/(RCB+RCE);
+    lbl5:
+           if (P<eps) P=eps;
+
+           PPB=P/PB;
+           if(PB>P) goto lbl1;
+           PKB=PGAM*PPB+OGAM;
+           ZNB=RCB*sqrt(PKB);
+           F1=(P-PB)/ZNB ;
+           FS1=(QGAM*PPB+FGAM)/(RGAM*ZNB*PKB);
+           goto lbl2;
+    lbl1:      
+           ZFB=CB*exp(log(PPB)*OGAM) ;
+           F1=DGAM*(ZFB-CB);
+           FS1=ZFB/(GAM*P);
+    lbl2:      
+           PPE=P/PE;
+           if(PE>P) goto lbl3;
+           PKE=PGAM*PPE+OGAM;
+           ZNE=RCE*sqrt(PKE);
+           F2=(P-PE)/ZNE;
+           FS2=(QGAM*PPE+FGAM)/(RGAM*ZNE*PKE);
+           goto lbl4;
+    lbl3:      
+           ZFE=CE*exp(log(PPE)*OGAM);
+           F2=DGAM*(ZFE-CE);
+           FS2=ZFE/(GAM*P);
+    lbl4:      
+           DP=(DU-F1-F2)/(FS1+FS2);
+           P=P+DP;
+       if(fabs(DU-F1-F2)>eps) goto lbl5;
+ 
+
+      PPB=P/PB;
+      PPE=P/PE;
+
+//       ZFB=CB*PPB**OGAM;
+//       ZFE=CE*PPE**OGAM;
+      ZFB=CB*exp(log(PPB)*OGAM);
+      ZFE=CE*exp(log(PPE)*OGAM);
+      if (PB>P) goto lbl6 ;
+      D=UB-sqrt((TGAM*P+HGAM*PB)/RB);
+      UBD=UB-D;
+      RUBD=RB*UBD;
+      RF=RUBD*RUBD/(PB-P+RUBD*UBD);
+      UF=D+RUBD/RF;
+      EF=P/(AGAM*RF);
+      SBL=D;
+      SFL=D;
+      goto lbl7;
+    lbl6: 
+      EF=ZFB*ZFB/SGAM;
+      UF=UB+DGAM*(CB-ZFB);
+      RF=P/(AGAM*EF);
+      SBL=UB-CB;
+      SFL=UF-ZFB;
+    lbl7: 
+      if (PE>P) goto lbl8;
+      D=UE+sqrt((TGAM*P+HGAM*PE)/RE);
+      UED=UE-D;
+      RUED=RE*UED;
+      RS=RUED*RUED/(PE-P+RUED*UED);
+      US=D+RUED/RS;
+      ES=P/(AGAM*RS);
+      SEL=D;
+      SSL=D;
+      goto lbl9;
+    lbl8: 
+      ES=ZFE*ZFE/SGAM;
+      US=UE-DGAM*(CE-ZFE);
+      RS=P/(AGAM*ES);
+      SSL=US+ZFE ;
+      SEL=UE+CE ;
+    lbl9: 
 // 
-// 	double US = 0.0, UF = 0.0;
-// 
-// 	double RF,RS,EF,ES,SBL,SFL,SSL,SEL,D;
-// 	double    eps=1.0e-5;
-// 	double    CB= sqrt(GAM*PB/RB);
-// 	double    CE= sqrt(GAM*PE/RE);
-// 	double    EB= CB*CB/SGAM;
-// 	double    EE= CE*CE/SGAM;
-// 	double    RCB=RB*CB;
-// 	double    RCE=RE*CE;
-// 	double    DU=UB-UE;
-//       if (DU < -2.0*(CB+CE)/AGAM) {
-//          //printf(" ATTENTION!!!  VACUUM \n");
-//          RF=0.0;
-//          RS=0.0;
-//          EF=0.0;
-//          ES=0.0;
-//          SBL=UB-CB;
-//          SFL=UB+2.0*CB/AGAM;
-//          SSL=UE-2.0*CE/AGAM;
-//          SEL=UE+CE;
-//          goto lbl9;
-//       }
-//       double P=(PB*RCE+PE*RCB+DU*RCB*RCE)/(RCB+RCE);
-//     lbl5:
-//            if (P<eps) P=eps;
-// 
-//            double PPB=P/PB;
-//            if(PB>P) goto lbl1;
-//            double PKB=PGAM*PPB+OGAM;
-//            double ZNB=RCB*sqrt(PKB);
-//            double F1=(P-PB)/ZNB ;
-//            double FS1=(QGAM*PPB+FGAM)/(RGAM*ZNB*PKB);
-//            goto lbl2;
-//     lbl1:      
-//            double ZFB=CB*exp(log(PPB)*OGAM) ;
-//            F1=DGAM*(ZFB-CB);
-//            FS1=ZFB/(GAM*P);
-//     lbl2:      
-//            double PPE=P/PE;
-//            if(PE>P) goto lbl3;
-//            double PKE=PGAM*PPE+OGAM;
-//            double ZNE=RCE*sqrt(PKE);
-//            double F2=(P-PE)/ZNE;
-//            double FS2=(QGAM*PPE+FGAM)/(RGAM*ZNE*PKE);
-//            goto lbl4;
-//     lbl3:      
-//            double ZFE=CE*exp(log(PPE)*OGAM);
-//            F2=DGAM*(ZFE-CE);
-//            FS2=ZFE/(GAM*P);
-//     lbl4:      
-//            double DP=(DU-F1-F2)/(FS1+FS2);
-//            P=P+DP;
-//        if(fabs(DU-F1-F2)>eps) goto lbl5;
-//  
-// 
-//       PPB=P/PB;
-//       PPE=P/PE;
-// 
-// //       ZFB=CB*PPB**OGAM;
-// //       ZFE=CE*PPE**OGAM;
-//       ZFB=CB*exp(log(PPB)*OGAM);
-//       ZFE=CE*exp(log(PPE)*OGAM);
-//       if (PB>P) goto lbl6 ;
-//       D=UB-sqrt((TGAM*P+HGAM*PB)/RB);
-//       double UBD=UB-D;
-//       double RUBD=RB*UBD;
-//       RF=RUBD*RUBD/(PB-P+RUBD*UBD);
-//       UF=D+RUBD/RF;
-//       EF=P/(AGAM*RF);
-//       SBL=D;
-//       SFL=D;
-//       goto lbl7;
-//     lbl6: 
-//       EF=ZFB*ZFB/SGAM;
-//       UF=UB+DGAM*(CB-ZFB);
-//       RF=P/(AGAM*EF);
-//       SBL=UB-CB;
-//       SFL=UF-ZFB;
-//     lbl7: 
-//       if (PE>P) goto lbl8;
-//       D=UE+sqrt((TGAM*P+HGAM*PE)/RE);
-//       double UED=UE-D;
-//       double RUED=RE*UED;
-//       RS=RUED*RUED/(PE-P+RUED*UED);
-//       US=D+RUED/RS;
-//       ES=P/(AGAM*RS);
-//       SEL=D;
-//       SSL=D;
-//       goto lbl9;
-//     lbl8: 
-//       ES=ZFE*ZFE/SGAM;
-//       US=UE-DGAM*(CE-ZFE);
-//       RS=P/(AGAM*ES);
-//       SSL=US+ZFE ;
-//       SEL=UE+CE ;
-//     lbl9: 
-// // 
-// // C     compute the interpolation value
-//       if (SEL<=0.0) {
-//          RI= RE;
-//          EI= EE;
-//          UI= UE;
-//          VI= VE;
-//          WI= WE;
-//          goto lbl157;
-//       }
-// 
-//       if (SBL>=0.0) {
-//          RI= RB;
-//          EI= EB;
-//          UI= UB;
-//          VI= VB;
-//          WI= WB;
-//          goto lbl157;
-//       }
-// 
-//       if ((SSL>=0.0)&&(SFL<=0.0)) {
-//          if (US>=0.0) {
-//             RI= RF;
-//             EI= EF;
-//             UI= UF;
-//             VI= VB;
-//             WI= WB;
-//          } else {
-//             RI= RS;
-//             EI= ES;
-//             UI= US;
-//             VI= VE;
-//             WI= WE;
-//          }
-//          goto lbl157;
-//       }
-// 
-//       if (SFL>0.0) {
-//          UI= (UB+DGAM*GGAM*sqrt(EB))/(1+DGAM);
-//          VI= VB;
-// 		 WI= WB;
-// 		 EI= (UI*UI)/SGAM;
-//          RI= RB*exp(log(EI/EB)*(1/AGAM));
-//        } else {
-//          UI= (UE-DGAM*GGAM*sqrt(EE))/(1+DGAM);
-//          VI= VE;
-// 		 WI= WE;
-// 		 EI= (UI*UI)/SGAM;
-//          RI= RE*exp(log(EI/EE)*(1/AGAM)) ;
-//       }
-// 
-//   lbl157:
-//       PI= AGAM*EI*RI;
+// C     compute the interpolation value
+      if (SEL<=0.0) {
+         RI= RE;
+         EI= EE;
+         UI= UE;
+         VI= VE;
+         WI= WE;
+         goto lbl157;
+      }
+
+      if (SBL>=0.0) {
+         RI= RB;
+         EI= EB;
+         UI= UB;
+         VI= VB;
+         WI= WB;
+         goto lbl157;
+      }
+
+      if ((SSL>=0.0)&&(SFL<=0.0)) {
+         if (US>=0.0) {
+            RI= RF;
+            EI= EF;
+            UI= UF;
+            VI= VB;
+            WI= WB;
+         } else {
+            RI= RS;
+            EI= ES;
+            UI= US;
+            VI= VE;
+            WI= WE;
+         }
+         goto lbl157;
+      }
+
+      if (SFL>0.0) {
+         UI= (UB+DGAM*GGAM*sqrt(EB))/(1+DGAM);
+         VI= VB;
+		 WI= WB;
+		 EI= (UI*UI)/SGAM;
+         RI= RB*exp(log(EI/EB)*(1/AGAM));
+       } else {
+         UI= (UE-DGAM*GGAM*sqrt(EE))/(1+DGAM);
+         VI= VE;
+		 WI= WE;
+		 EI= (UI*UI)/SGAM;
+         RI= RE*exp(log(EI/EE)*(1/AGAM)) ;
+      }
+
+  lbl157:
+      PI= AGAM*EI*RI;
  
 } 
 
